42bc9e92d59a2a2a76507988bae92dab4d646f4b
Abe Timmerman <abeltje@cpan.org>
2020-08-03 11:13:45 +0200

    use Moo; not Moose...

--- t/lib/MyTest/Exception.pm
+++ t/lib/MyTest/Exception.pm
@@ -1,12 +1,12 @@
 package MyTest::Exception;
-use Moose;
+use Moo;
+use Types::Standard qw( Str );
 
 use overload '""' => 'as_string';
 
-has error => (is => 'ro', isa => 'Str');
+has error => (is => 'ro', isa => Str);
 
 sub as_string { $_[0]->error }
 
 use namespace::autoclean;
-__PACKAGE__->meta->make_immutable();
 1;
