b9b6a625163e8d710570fb8ce157f76696b4a9f5
Abe Timmerman <abeltje@cpan.org>
2020-08-12 15:43:34 +0200

    Autocommit for distribution Dancer::Plugin::RPC 1.08_05 (test)

--- lib/Dancer/Plugin/RPC.pm
+++ lib/Dancer/Plugin/RPC.pm
@@ -2,7 +2,7 @@ package Dancer::Plugin::RPC;
 use warnings;
 use strict;
 
-our $VERSION = '1.08_04';
+our $VERSION = '1.08_05';
 
 # Will be set from the request-handler for the callback scope
 our $ROUTE_INFO = {
